ae67e51882ae1eb9f264ce28312049061b8fbbba
d_fujimoto <d_fujimoto@beeworks.co.jp>
2021-08-10 13:05:23 +0900

    fix:for ofはieでは使えない

--- gulpfile.js
+++ gulpfile.js
@@ -76,7 +76,7 @@ function buildJs() {
     {
       // モード値を production に設定すると最適化された状態で、
       // development に設定するとソースマップ有効でJSファイルが出力される
-      mode: 'development',
+      mode: 'production',
 
       entry: entryObj,
       output: {
--- src/js/project/_modal.js
+++ src/js/project/_modal.js
@@ -1,18 +1,32 @@
 export default class {
   constructor() {
-    const modalButton = Array.prototype.slice.call(document.querySelectorAll('.modal__button'));
-    const modalBackground = Array.prototype.slice.call(document.querySelectorAll('.modal__background'));
-    for (let modalTarget of modalButton) {
+    const modalButton = Array.prototype.slice.call(document.getElementsByClassName('modal__button'), 0);
+    const modalBackground = Array.prototype.slice.call(document.querySelectorAll('.modal__background'), 0);
+    // const modalButton = Array.prototype.slice.call(document.querySelectorAll('.modal__button'));
+    // const modalBackground = Array.prototype.slice.call(document.querySelectorAll('.modal__background'));
+    modalButton.forEach((modalTarget) => {
       modalTarget.addEventListener('click', () => {
         const target = modalTarget.dataset.target;
         const targetElement = document.querySelector('#' + target);
         targetElement.classList.add('is-show');
       });
-    }
-    for (let modalClose of modalBackground) {
+    });
+    modalBackground.forEach((modalClose) => {
       modalClose.addEventListener('click', () => {
-        document.querySelector('.is-show').classList.remove('is-show');
+        document.querySelector('.modal.is-show').classList.remove('is-show');
       });
-    }
+    });
+    // for (let modalTarget of modalButton) {
+    //   modalTarget.addEventListener('click', () => {
+    //     const target = modalTarget.dataset.target;
+    //     const targetElement = document.querySelector('#' + target);
+    //     targetElement.classList.add('is-show');
+    //   });
+    // }
+    // for (let modalClose of modalBackground) {
+    //   modalClose.addEventListener('click', () => {
+    //     document.querySelector('.is-show').classList.remove('is-show');
+    //   });
+    // }
   }
 }
