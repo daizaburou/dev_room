4f93fb1e8fa10270791ee9d621e2a2547a94bab8
daizaburo <daizabro@gmail.com>
2022-03-13 22:36:04 +0900

    gulpからeleventyに変更

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const Image = require("@11ty/eleventy-img");
+
+(async () => {
+  let url = "https://images.unsplash.com/photo-1608178398319-48f814d0750c";
+  let stats = await Image(url, {
+    widths: [300]
+  });
+
+  // console.log( stats );
+})();
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    open:true,
+  })
+  return {
+    dir: {
+      input: 'src/html',
+      output: 'dist'
+    }
+  }
+}
