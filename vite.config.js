4868c325ea358d9d130a62746f05892b81318c13
daizaburo <daizabro@gmail.com>
2022-03-14 02:18:20 +0900

    コマンド追加

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,19 @@ export default defineConfig({
     },
     rollupOptions:{
       input: Object.fromEntries(
-        glob.sync("{js,scss}/**/*.{js,scss}",{
+        glob.sync("{js,css}/**/*.{js,scss}",{
           ignore: "**/_**/*.{js,scss}",
           cwd: `./src`,
         }).map((file)=>{
           const { dir,name } = path.parse(file);
           return [`${dir}/${name}`,path.resolve("src",file)];
         })
-      )
+      ),
+      output:{
+        entryFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+        manualChunks: undefined,
+      }
     }
   },
 })
